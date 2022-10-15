44615573e122b282ac2402cb73447b78e77da88a
“preetimca2016” <preetimca2016@gmail.com>
2022-10-16 00:41:25 +0530

    Added mobile number validation

--- Html Form and Validation/js/FormValidation.js
+++ Html Form and Validation/js/FormValidation.js
@@ -36,6 +36,23 @@ text.addEventListener('input',function()
             emailError.textContent = "Email is invaild"
         }
     });
+    //uc3 -Mobile numbvalidating mobile number
+    //Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
+const tel = document.querySelector('#tel');
+const telError = document.querySelector('.tel-error');
+tel.addEventListener('input', function () 
+{
+    let telRegex = RegExp('^[0-9]{2}[ ][0-9]{10}$');
+    //Checking number is valid or not using if else condition
+    if (telRegex.test(tel.value)) 
+    {
+        telError.textContent = "";
+    }
+    else 
+    {
+        telError.textContent = "telephone number is not Valid";
+    }
+});
    
 
    
