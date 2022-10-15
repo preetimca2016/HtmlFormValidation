14a8e79311e36d338cff43944a62f0a6e1ca9779
“preetimca2016” <preetimca2016@gmail.com>
2022-10-15 23:00:12 +0530

    Added html form

---
+++ Html Form and Validation/js/FormValidation..js
@@ -0,0 +1,17 @@
+var text = document.querySelector("#name");
+console.log(text);
+var error = document.querySelector(".error");
+text.addEventListener('input',function()
+{
+    var nameRegex=RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(text.value))
+    {
+        error.textContent="";
+    }
+    else
+        error.textContent="Name INVALID";
+       
+    });
+
+
+   
