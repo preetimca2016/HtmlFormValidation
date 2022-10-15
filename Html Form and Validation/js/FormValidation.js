87d3ae45c673dd12574e8f607058c6a28dbff15c
“preetimca2016” <preetimca2016@gmail.com>
2022-10-16 00:14:41 +0530

    Added email validaation i form

--- Html Form and Validation/js/FormValidation.js
+++ Html Form and Validation/js/FormValidation.js
@@ -1,3 +1,12 @@
+//Button Range
+var salaryInput = document.querySelector('#salary');
+var output = document.querySelector('.salary-error')
+output.textContent = salaryInput.value;
+salaryInput.addEventListener('input', function(){
+    output.textContent = salaryInput.value;
+});
+
+//UC1:Name Validation
 var text = document.querySelector("#name");
 console.log(text);
 var error = document.querySelector(".error");
@@ -12,11 +21,21 @@ text.addEventListener('input',function()
         error.textContent="Name INVALID";
        
     });
-    var salaryInput = document.querySelector('#salary');
-    var output = document.querySelector('.salary-error')
-    output.textContent = salaryInput.value;
-    salaryInput.addEventListener('input', function(){
-        output.textContent = salaryInput.value;
+    //UC2:Email Validation
+    const Email = document.querySelector('#email');
+    const emailError = document.querySelector('.email-error');
+    emailError.addEventListener('input',function(){
+        let emailRegex = RegExp("^[A-Za-z]{3,}([.+_-][A-Za-z0-9]+)*[@][A-Za-z0-9]{1,}[.][a-zA-Z0-9]{2,3}([.][0-9a-zA-Z]{2})?$"); 
+        //checking email is valid or not.
+        if(emailRegex.test(email.value))
+        {
+            emailError.textContent="";
+        }
+        else
+        {
+            emailError.textContent = "Email is invaild"
+        }
     });
+   
 
    
