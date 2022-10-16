cd73acb79f00a8cbfc988a012741f95e733c6e2c
“preetimca2016” <preetimca2016@gmail.com>
2022-10-16 16:32:44 +0530

    [Preeti]Add:Password Validation

--- Html Form and Validation/js/FormValidation.js
+++ Html Form and Validation/js/FormValidation.js
@@ -36,6 +36,35 @@ text.addEventListener('input',function()
             emailError.textContent = "Email is invaild"
         }
     });
-   
-
+    //uc3 -Mobile numbvalidating mobile number
+    //Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
+const tel = document.querySelector('#tel');
+const telError = document.querySelector('.tel-error');
+tel.addEventListener('input', function () 
+{
+    let telRegex = RegExp('^[0-9]{2}[ ][0-9]{10}$');
+    //Checking number is valid or not using if else condition
+    if (telRegex.test(tel.value)) 
+    {
+        telError.textContent = "";
+    }
+    else 
+    {
+        telError.textContent = "telephone number is not Valid";
+    }
+});
+ //UC5: Follow All rule For Password like one splecial char, atleast one digit capital letter and 1 numeric number in the password  
+ const pwd = document.querySelector('#pwd');
+ const pwdError = document.querySelector('.pwd-error');
+ pwd.addEventListener('input', function () 
+ {
+     let pwdRegex = RegExp('^(?=.*[0-9])(?=.*[A-Z])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$');
+     //checking password is valid or not by using if else condition
+     if (pwdRegex.test(pwd.value)) 
+     {
+         pwdError.textContent = "";
+     }
+     else 
+         pwdError.textContent = "password is not Valid";
+      });
    
